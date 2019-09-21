c0b7f1217d6396c30f93977d80514bd6c69509f3
seeris <sergei.udris@gmail.com>
2019-09-21 11:10:48 +0300

    data

--- src/stage1/app/src/app/data/core.clj
+++ src/stage1/app/src/app/data/core.clj
@@ -1,24 +1,44 @@
 (ns app.data.core
   (:require [clojure.repl :refer :all]
-            [clojure.java.shell :refer [sh] :as shell]))
+            [clojure.java.shell :refer [sh] :as shell]
+            [clojure.java.io :as io]))
+
+; (prn "---using .java.shell to list directory:")
+; (sh "ls /")
+
+(def data-dir "/opt/data/")
+
+ (when-not (.exists (io/file (str data-dir "sr28asc")))
+   (sh "bash "))
 
 (comment
 
-  (shell/sh "ls")
+  (sh "ls")
 
-  (shell/sh "pwd")
+  (sh "pwd" :dir "/opt/data")
 
-  (shell/sh "echo 3")
+  (sh "ls" :dir "/opt")
+  (sh "ls" :dir "/opt/data")
+  (sh "ls" :dir "/opt/app")
+
+  (sh "ls" :dir "/opt/data")
+
+  (sh "pwd")
+  
+  (sh "bash")
+
   shell/*sh-dir*
 
   (shell/with-sh-dir)
 
   (shell/sh "lein compile")
 
+  (println (:out (sh "cowsay" "Printing a command-line output")))
+
   (sh "echo $(pwd)")
   (sh "bash -c \"ls\"")
   (sh "ls /usr/bin/java")
-  
+
 
   (when-not (.exists (io/file (str data-dir)))
     (do (println "Retrieving data ...") (sh "./get_data.sh")))
