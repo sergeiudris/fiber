b65e12a3dc4740119aa5360b2e36fb85d212f340
seeris <sergei.udris@gmail.com>
2019-09-22 17:00:36 +0300

    sample table

--- src/stage1/ui/src/ui/count/sample.cljs
+++ src/stage1/ui/src/ui/count/sample.cljs
@@ -15,14 +15,14 @@
     :address "10 Downing Street"}
    {:key "2"
     :name "John"
-    :age 42
+    :age 43
     :address "11 Downing Street"}
    ]
   )
 
 (def columns
   [{:title "Name"
-    :dataIndex "name"
+    :dataIndex :name
     :key "name"}
    {:title "Age"
     :dataIndex "age"
@@ -59,6 +59,39 @@
   (let []
     (fn []
       [ant-table
-       {:dataSource data-source-2
-        :columns columns-2
+       {:dataSource data-source
+        :columns columns
         }])))
+
+
+
+;; people data
+#_(def people-data [{:id 1 :name "Tracey Davidson" :age 43 :address "5512 Pockrus Page Rd"}
+                    {:id 2 :name "Pierre de Wiles" :age 41 :address "358 Fermat's St"}
+                    {:id 3 :name "Lydia Weaver" :age 23 :address "1251 Fourth St"}
+                    {:id 4 :name "Willie Reynolds" :age 26 :address "2984 Beechcrest Rd"}
+                    {:id 5 :name "Richard Perelman" :age 51 :address "2003 Poincaré Ricci Rd"}
+                    {:id 6 :name "Srinivasa Ramanujan" :age 32 :address "1729 Taxi Cab St"}
+                    {:id 7 :name "Zoe Cruz" :age 31 :address "8593 Pine Rd"}
+                    {:id 8 :name "Adam Turing" :age 41 :address "1936 Automata Lane"}])
+
+#_(defn comparison [data1 data2 field]
+    (compare (get (js->clj data1 :keywordize-keys true) field)
+             (get (js->clj data2 :keywordize-keys true) field)))
+
+
+;; we need to use dataIndex instead of data-index, see README.md
+#_(def people-columns [{:title "Name" :dataIndex "name" :sorter #(comparison %1 %2 :name)}
+                       {:title "Age" :dataIndex "age" :sorter #(comparison %1 %2 :age)}
+                       {:title "Address" :dataIndex "address" :sorter #(comparison %1 %2 :address)}])
+
+#_(def pagination {:show-size-changer true
+                   :default-page-size 10
+                   :page-size-options ["5" "10" "20"]
+                   :position "top"
+                   :show-total #(str "Total: " % " entities")})
+
+
+#_(def columns [{:title "attr1" :data-index "attr1" :key "attr1"}
+                {:title "attr2" :data-index "attr2" :key "attr2"}
+                {:title "attr3" :data-index "attr3" :key "attr3"}])
--- src/stage1/ui/src/ui/count/view.cljs
+++ src/stage1/ui/src/ui/count/view.cljs
@@ -26,12 +26,6 @@
 
 (def ant-table (r/adapt-react-class AntTable))
 
-
-(comment
-  
-  ;
-  )
-
 (defn stateful-comp
   []
   (let [component-state (r/atom {:count 0})]
@@ -40,9 +34,6 @@
        [:p "count is: " (get @component-state :count)]
        [:button {:on-click #(swap! component-state update-in [:count] inc)} "Increment"]])))
 
-#_(def columns [{:title "attr1" :data-index "attr1" :key "attr1"}
-              {:title "attr2" :data-index "attr2" :key "attr2"}
-              {:title "attr3" :data-index "attr3" :key "attr3"}])
 
 (def food-des-keywords
   [:usda.item/group-id
@@ -61,37 +52,11 @@
   )
 
 
-;; people data
-#_(def people-data [{:id 1 :name "Tracey Davidson" :age 43 :address "5512 Pockrus Page Rd"}
-             {:id 2 :name "Pierre de Wiles" :age 41 :address "358 Fermat's St"}
-             {:id 3 :name "Lydia Weaver" :age 23 :address "1251 Fourth St"}
-             {:id 4 :name "Willie Reynolds" :age 26 :address "2984 Beechcrest Rd"}
-             {:id 5 :name "Richard Perelman" :age 51 :address "2003 Poincaré Ricci Rd"}
-             {:id 6 :name "Srinivasa Ramanujan" :age 32 :address "1729 Taxi Cab St"}
-             {:id 7 :name "Zoe Cruz" :age 31 :address "8593 Pine Rd"}
-             {:id 8 :name "Adam Turing" :age 41 :address "1936 Automata Lane"}])
-
-#_(defn comparison [data1 data2 field]
-  (compare (get (js->clj data1 :keywordize-keys true) field)
-           (get (js->clj data2 :keywordize-keys true) field)))
-
-
-;; we need to use dataIndex instead of data-index, see README.md
-#_(def people-columns [{:title "Name" :dataIndex "name" :sorter #(comparison %1 %2 :name)}
-              {:title "Age" :dataIndex "age" :sorter #(comparison %1 %2 :age)}
-              {:title "Address" :dataIndex "address" :sorter #(comparison %1 %2 :address)}])
-
-#_(def pagination {:show-size-changer true
-                 :default-page-size 10
-                 :page-size-options ["5" "10" "20"]
-                 :position "top"
-                 :show-total #(str "Total: " % " entities")})
-
 (def food-des-columns
   (mapv (fn [kw]
           {:title (name kw)
            :key (name kw)
-           :dataIndex (name kw)
+           :dataIndex kw
           ;  :render (fn [txt rec idx]
           ;            txt)
            })
@@ -112,7 +77,7 @@
                         :size "small"
                         :row-key "key"
                         :columns columns
-                        :data-source (clj->js ents) }]
+                        :data-source ents }]
             nil))
         )))
 
@@ -187,6 +152,6 @@
        [buttons]
        [:br]
        [:br]
-       #_[table]
-       [ui.count.sample/sample-table]])))
+       [table]
+       #_[ui.count.sample/sample-table]])))
 
