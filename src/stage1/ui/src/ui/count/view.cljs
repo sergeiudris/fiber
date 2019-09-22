7c90d5d05996a385da6fc1b0f1a27e9128042291
seeris <sergei.udris@gmail.com>
2019-09-22 11:36:57 +0300

    ui

--- src/stage1/ui/src/ui/count/view.cljs
+++ src/stage1/ui/src/ui/count/view.cljs
@@ -5,10 +5,14 @@
              [re-frame.core :as rf]
              [ui.count.subs :as subs]
              [ui.count.events :as events]
-             ["antd/lib/button" :default ant-Button]
+             ["antd/lib/button" :default AntButton]
+             ["antd/lib/input/Search" :default AntInputSearch]
+             ["antd/lib/table" :default AntTable]
              ))
 
 
+(def ant-input-search (r/adapt-react-class AntInputSearch))
+(def ant-table (r/adapt-react-class AntTable))
 
 
 (comment
@@ -24,9 +28,33 @@
        [:p "count is: " (get @component-state :count)]
        [:button {:on-click #(swap! component-state update-in [:count] inc)} "Increment"]])))
 
-(defn count-panel []
+(def columns [{:title "attr1" :data-index "attr1" :key "attr1"}
+              {:title "attr2" :data-index "attr2" :key "attr2"}
+              {:title "attr3" :data-index "attr3" :key "attr3"}])
+
+(defn table
+    []
+    (let []
+      (fn []
+        [ant-table {:size "small"
+                    :columns columns 
+                    :data []}])))
+
+(defn search
+  []
+  (let []
+    (fn []
+      [ant-input-search {:style {:width "50%"}}])))
+
+(defn count-panel
+  []
   (let [module-count @(rf/subscribe [::subs/module-count])
-        base-url @(rf/subscribe [:ui.subs/base-url] )
-        ]
-    [:div "count"]))
+        base-url @(rf/subscribe [:ui.subs/base-url])]
+    (fn []
+      [:section
+       [search]
+       [:br]
+       [:br]
+       [:br]
+       [table]])))
 
