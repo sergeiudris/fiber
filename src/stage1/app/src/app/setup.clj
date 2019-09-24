6db5daf67194bfb1d9d36fc28cdcfc7bda0498d2
seeris <sergei.udris@gmail.com>
2019-09-24 16:08:41 +0300

    clj git clone

--- src/stage1/app/src/app/setup.clj
+++ src/stage1/app/src/app/setup.clj
@@ -3,9 +3,25 @@
             [app.db.core :as db]
             [datomic.api :as d]
             [app.db.query :as dbq]
-            [app.data.usda]))
+            [app.data.usda]
+            [clojure.java.shell :refer [sh]]
+            [clojure.java.io :as io]))
 
 (def *stage* (System/getenv "STAGE"))
+
+(defn download-data!
+  []
+  (try
+    (when (not (.exists (io/file "/opt/data")))
+      (do
+        (sh "/bin/bash" "-c"
+            "git clone https://github.com/seeris/fiber.data /opt/data/fiber.data"
+            :dir "/opt")))
+    (catch Exception e (do (prn (.getMessage e)) false))))
+
+#_(download-data!)
+#_(sh "/bin/bash" "-c" "ls /opt/data1/fiber.data" :dir "/opt")
+
 
 (defn db-populated?
   []
@@ -19,6 +35,7 @@
 
 (defn init!
   []
+  (download-data!)
   (app.data.usda/del-files!)
   (app.data.usda/create-files!)
   (app.db.core/populate!))
